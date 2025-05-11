aa6d556bbfa3450afff054dd3270c301dff92c4b
Roy Mainfeld <rmainfeld@rmainfeld-mbp.corp.lightricks.com>
2025-05-11 17:45:10 +0300

    run in collab

--- ex1.py
+++ ex1.py
@@ -8,9 +8,9 @@ from transformers.data.data_collator import DataCollatorWithPadding
 from datasets import load_dataset, DatasetDict, Dataset
 import numpy as np
 import wandb
+import torch
 
 wandb.login()
-wandb.init(project="ex1")
 
 @dataclass
 class DataArguments:
@@ -37,12 +37,16 @@ def compute_metrics(p: EvalPrediction):
     return {"accuracy": (preds == p.label_ids).mean()}
 
 
-def train(data_args: DataArguments, training_args: TrainingArguments, dataset: DatasetDict):
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+def train(
+        data_args: DataArguments,
+        training_args: TrainingArguments,
+        dataset: DatasetDict
+        ):
     
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
     def preprocess_function(examples):
         return tokenizer(examples["sentence1"], examples["sentence2"], truncation=True)
-    
+
     train_dataset = build_dataset("train", data_args.max_train_samples, dataset).map(preprocess_function, batched=True)
     eval_dataset = build_dataset("validation", data_args.max_eval_samples, dataset).map(preprocess_function, batched=True)
 
@@ -59,35 +63,71 @@ def train(data_args: DataArguments, training_args: TrainingArguments, dataset: D
         data_collator=data_collator
     )
 
-    train_result = trainer.train()
-    print(f"Training loss: {train_result.training_loss}")
-    print(f"Steps: {train_result.global_step}")
-    print(f"Metrics: {train_result.metrics}")
+    trainer.train()
+    trainer.save_model("saved_model")
 
+    eval_metrics = trainer.evaluate()
 
-def predict():
-    pass
+    return eval_metrics["eval_accuracy"]
+
+def predict(
+        data_args: DataArguments,
+        dataset: DatasetDict
+        ):
+    
+    tokenizer = AutoTokenizer.from_pretrained(data_args.model_path)
+    def preprocess_function(examples):
+        return tokenizer(examples["sentence1"], examples["sentence2"], truncation=True, return_tensors="pt")
+        
+    model = AutoModelForSequenceClassification.from_pretrained(data_args.model_path)
+
+    test_set: Dataset = dataset["test"]
+    if data_args.max_predict_samples > 0:
+        test_set = test_set.select(range(data_args.max_predict_samples))
+
+    model.eval()
+
+    str_to_write = ""
+
+    with torch.no_grad():
+        for sample in test_set:
+            model_inputs = preprocess_function(sample)
+            outputs = model(**model_inputs)
+            pred = torch.argmax(outputs.logits, dim=1).item()
+            str_to_write += f"{sample['sentence1']}###{sample['sentence2']}###{pred}\n" # type: ignore
+
+    with open("predictions.txt", "w") as f:
+        f.write(str_to_write)
+
 
 def main():
+
+
     parser = HfArgumentParser((TrainingArguments, DataArguments, AdditionalTrainingArguments)) # type: ignore
     training_args, data_args, additional_training_args = parser.parse_args_into_dataclasses()
+    wandb.init(project="ex1",
+               name=f"batch_size_{additional_training_args.batch_size}_lr_{additional_training_args.lr}",
+               config = {
+                "batch_size": additional_training_args.batch_size,
+                "lr": additional_training_args.lr
+               })
 
     training_args.per_device_train_batch_size = additional_training_args.batch_size
     training_args.learning_rate = additional_training_args.lr
     training_args.report_to = "wandb"
     training_args.logging_strategy = "steps"
     training_args.logging_steps = 1
-    training_args.logging_dir = "./logs"
-
-
 
     dataset = load_dataset("nyu-mll/glue", "mrpc")
-    
-    if training_args.do_train:
-        train(data_args, training_args, dataset) # type: ignore
 
+    if training_args.do_train:
+        eval_accuracy = train(data_args, training_args, dataset) # type: ignore
+
+        with open("res.txt", "a") as f:
+            f.write(f"epoch_num : {training_args.num_train_epochs}, lr : {additional_training_args.lr}, batch_size : {additional_training_args.batch_size}, eval_accuracy : {eval_accuracy}\n")
+                    
     if training_args.do_predict:
-        predict()
+        predict(data_args, dataset) # type: ignore
 
     wandb.finish()
 
